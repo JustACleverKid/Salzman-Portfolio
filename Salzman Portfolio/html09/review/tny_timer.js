eb236d9cfd808484fb7ad3d9bc60a257586f1140
Jack Salzman <jacksalzman120@gmail.com>
2020-09-15 19:28:36 -0600

    Made Changes

--- Salzman Portfolio/html09/review/tny_timer.js
+++ Salzman Portfolio/html09/review/tny_timer.js
@@ -1,14 +1,22 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Review Assignment
 
    Event Timer
-   Author: 
-   Date:   
+   Author: Jack Salzman
+   Date:   9.15.2020
 
 */
 
+function showClock() {
+   var thisDay = new Date("May 19, 2018 at 9:31:27 a.m.")
+   var localDate = new Date("May 19, 2018 at 9:31:27 a.m.")
+   var localTime = new Date("May 19, 2018 at 9:31:27 a.m.")
+}
+
+document.getElementById("currentTime").innerHTML = "<span>date</span><span>time</span>";
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
