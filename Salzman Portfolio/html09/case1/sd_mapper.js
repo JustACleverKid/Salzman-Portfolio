6e656ea9d0ee87c14184718f70765bd87eb0000e
Jack Salzman <jacksalzman120@gmail.com>
2020-09-17 20:43:50 -0600

    help pls

--- Salzman Portfolio/html09/case1/sd_mapper.js
+++ Salzman Portfolio/html09/case1/sd_mapper.js
@@ -21,8 +21,8 @@ var timeStr = thisTime.toLocaleString();
 
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
-var mapNum = (2*thisMonth + thisHour) % 24
-var imgStr = String("<img src='sd_sky"+mapNum+".png'>");
+var mapNum = (2 * thisMonth + thisHour) % 24
+var imgStr = String("<img src='sd_sky" + mapNum + ".png'>");
 
 //set id's to variables
 
