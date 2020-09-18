bb2564c7e477d454dbb69271b4b8d6ba4c541c89
Jack Salzman <jacksalzman120@gmail.com>
2020-09-17 19:43:18 -0600

    Added files

--- Salzman Portfolio/html09/case1/sd_mapper.js
+++ Salzman Portfolio/html09/case1/sd_mapper.js
@@ -1,11 +1,12 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 1
 
    Planisphere Script
-   Author: 
-   Date:   
+   Author: Jack Salzman
+   Date:   Thursday, September 17
 
 */
 
