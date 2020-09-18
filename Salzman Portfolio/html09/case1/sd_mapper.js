f8f68234c1ec6d8e3998ec4942ee44d32d9965aa
Jack Salzman <jacksalzman120@gmail.com>
2020-09-17 20:43:08 -0600

    I think I broke something...

--- Salzman Portfolio/html09/case1/sd_mapper.js
+++ Salzman Portfolio/html09/case1/sd_mapper.js
@@ -11,11 +11,20 @@
 
 */
 
-var thisTime = new Date("February 3, 2018 3:15:28");
+//define "thisTime" and write to "timeStr"
+
+var thisTime = new Date();
 var timeStr = thisTime.toLocaleString();
-document.getElementById("timestamp").innerHTML = timeStr;
+
+//use defined variables to make more variables
+
+
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
-var mapNum = ((2*thisMonth + thisHour) % 24)
-var imgStr = String("<img src='sd_sky" + mapNum + ".png'>")
+var mapNum = (2*thisMonth + thisHour) % 24
+var imgStr = String("<img src='sd_sky"+mapNum+".png'>");
+
+//set id's to variables
+
+document.getElementById("timestamp").innerHTML(timeStr);
 document.getElementById("planisphere").insertAdjacentHTML(imgStr);
