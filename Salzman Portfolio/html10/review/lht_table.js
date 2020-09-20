7c3cf38426e95e4eea310a3ec1b3f586cd535d88
Jack Salzman <jacksalzman120@gmail.com>
2020-09-20 17:03:53 -0600

    Added Var's and finished HTML

--- Salzman Portfolio/html10/review/lht_table.js
+++ Salzman Portfolio/html10/review/lht_table.js
@@ -11,3 +11,8 @@
 	
 */
 
+var thisDay = new Date("August 30, 2018");
+var tableHTML = (
+"<table id='eventTable'><caption>Upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>"
+);
+var endDate = new Date(thisDay.getTime() + 14*24*60*60*1000)
